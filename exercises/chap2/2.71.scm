10a33490bb1a856ab0e2d6bb398c0f6cf4cb95e4
Raphael Baron <rbaron@rbaron.net>
2015-09-15 17:36:29 -0300

    Finished 2.71

---
+++ exercises/chap2/2.71.scm
@@ -0,0 +1,8 @@
+; How many bits are needed to represent n symbols using Huffman trees?
+
+; The requests trees are sketched on:
+; - 2.71.a.jpg for n = 5;
+; - 2.72.b.jpg for n = 10.
+
+; As noted, 1 bit is necessary to represent the most frequent
+; symbol, and n-1 bits for the two least frequent symbols.
