7b17d2c260e76d984177f02391294fecfb45faec
Raphael Baron <rbaron@rbaron.net>
2016-05-13 19:44:50 -0300

    Finished 4.55

---
+++ exercises/chap4/4.55.scm
@@ -0,0 +1,13 @@
+; Examples of queries
+
+; all people supervised by Ben Bitdiddle;
+
+; => (supervisor ?name (Ben Bitwiddle))
+
+; b. the names and jobs of all people in the accounting division;
+
+; => (job ?name (accounting . ?job))
+
+; c. the names and addresses of all people who live in Slumerville.
+
+; => (address ?name (Slimerville . ?address))
