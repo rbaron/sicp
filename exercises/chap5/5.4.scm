be2bf1ae8cede738506a10523543a8c95761cd7f
Raphael Baron <rbaron@rbaron.net>
2016-06-23 20:11:12 -0300

    Finished 5.4

---
+++ exercises/chap5/5.4.scm
@@ -0,0 +1,52 @@
+; Register machines for
+
+; a. Recursive exponentiation
+
+(define (expt b n)
+  (if (= n 0)
+      1
+      (* b (expt b (- n 1)))))
+
+(controller
+    (assign continue (label done))
+  exp-loop
+    (test (op =) (reg n) (const 0))
+    (branch (label base-case))
+    (save continue)
+    (assign n (op -) (reg n) (const 1))
+    (assign continue (label ret))
+    (goto (label exp-loop))
+  ret
+    (restore continue)
+    (assign val (op *) (reg b) (reg val))
+    (goto (reg continue))
+  base-case
+    (assign val (const 1))
+    (goto (reg continue))
+  done)
+
+; b. Iterative exponentiation
+
+(define (expt b n)
+  (define (expt-iter counter product)
+    (if (= counter 0)
+        product
+        (expt-iter (- counter 1) (* b product))))
+  (expt-iter n 1))
+
+(controller
+    (assign counter (reg n))
+    (assign product (const 1))
+    (assign continue (label done))
+
+  expt-loop
+    (test (op =) (reg counter) (const 0))
+    (branch (label base-case))
+    (assign counter (op -) (reg counter) (const 1))
+    (assign counter (op *) (reg b) (reg product))
+    (goto (label expt-loop))
+  base-case
+    ; Do nothing. Result is already in register product.
+    ; No stack used! That's the reason iterative procedures
+    ; do not cause stack overflows (well, if tail rec opt is availble...)
+)
