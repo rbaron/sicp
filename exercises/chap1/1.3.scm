b5211e701a4658a63bc064dea45acd6d13f0fdad
Raphael Baron <rbaron@rbaron.net>
2016-08-07 10:50:47 -0300

    Fixed 1.3

--- exercises/chap1/1.3.scm
+++ exercises/chap1/1.3.scm
@@ -1,8 +1,17 @@
 (define (square x) (* x x))
 
+; Without min/max
 (define (sum-quares-two-greatest a b c)
-  (+ (square (if (> a b) a b))
-     (square (if (> b c) b c))))
+  (let ((greatest-first-2 (if (> a b) a b))
+        (smallest-first-2 (if (> a b) b a)))
+    (let ((second-greatest (if (> c smallest-first-2) c smallest-first-2)))
+      (+ (square greatest-first-2)
+         (square second-greatest)))))
+
+; With min/max
+(define (sum-quares-two-greatest a b c)
+  (+ (square (max a b))
+     (square (max c (min a b)))))
 
 ;Tests
 ;=====
